dc74024becdea42db8d287cd0d7c3065b978adea
SamOgon <sema.kraevoy@yandex.ru>
2020-11-08 23:30:18 +0300

    2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Avatar :src="image" style="width: 300px; height: 300px; border: 1px solid black" @miniAvatar="setMiniAvatar"/>
+        <input type="file" @change="loadImage">
+        <img :src="miniAvatar" width="100px" height="100px" style="border-radius: 50px"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Avatar from "./components/Avatar";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Avatar
+        },
+        data() {
+            return {
+                image: {},
+                miniAvatar: ""
+            }
+        },
+        methods: {
+            loadImage(e) {
+                this.image = URL.createObjectURL(e.target.files[0]);
+            },
+            setMiniAvatar(src) {
+                this.miniAvatar = src;
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    html, body {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+
+    #app {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
